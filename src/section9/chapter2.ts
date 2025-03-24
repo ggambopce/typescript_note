c5ec6a5efa1f6febea7b085accdbfbf5447242d7
ggambopce <ggambopce@gmail.com>
2025-03-24 15:41:32 +0900

    ✨ 분산적인 조건부 타입

---
+++ src/section9/chapter2.ts
@@ -0,0 +1,31 @@
+ /**
+  * 분산적인 조건부 타입
+  */
+
+ type StringNumberSwitch<T> = T extends number ? string : number;
+
+ let a: StringNumberSwitch<number>;
+
+ let b: StringNumberSwitch<string>;
+
+ let c: StringNumberSwitch<number | string>;
+ // StringNumberSwitch<number> | StringNumberSwitch<string>
+ // --> string | number
+
+ let d: StringNumberSwitch<boolean | number | string>;
+  // number | string
+
+/**
+  * 실용적인 예제
+  */
+
+type Exclude<T, U> = T extends U ? never : T;
+
+type A = Exclude<number | string | boolean, string>;
+// number | nerver | boolean
+// number | boolean 
+
+type Extract<T, U> = T extends U ? T : never;
+type B = Extract<number | string | boolean, string>;
+// string
+
